4d31ec8a22194df60fb77509a32df9bbd9332131
Ashish_Amar_Gaikwad <ashishgaikwad@accucia.co>
2022-09-03 10:03:55 +0530

    Add winner player functionality

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ const currentE2 = document.getAnimations('current--1');
 let currentScore = 0;
 let activePlayer = 0;
 const scores = [0, 0];
-console.log(currentE1);
 
 score1E1.textContent = 0;
 score2E2.textContent = 0;
@@ -25,29 +24,58 @@ const holdBtn = document.querySelector('.btn--hold');
 const playerStyleE1 = document.querySelector('.player--0');
 const playerStyleE2 = document.querySelector('.player--1');
 
+let playing = true;
+const switchPlayer = () => {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+  playerStyleE1.classList.toggle('player--active');
+  playerStyleE2.classList.toggle('player--active');
+};
+
 rollDiceBtn.addEventListener('click', () => {
-  // 1 generate random number
-  const rollDice = Math.trunc(Math.random() * 6) + 1;
-  // 2 showing the dice
-  diceEl.classList.remove('hidden');
-  diceEl.src = `dice-${rollDice}.png`;
+  if (playing) {
+    // 1 generate random number
+    const rollDice = Math.trunc(Math.random() * 6) + 1;
+    // 2 showing the dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${rollDice}.png`;
 
-  // 3 checking numbers is not zero
-  if (rollDice !== 1) {
-    // Add dice to current score
-    currentScore += rollDice;
-    // currentE1.textContent = currentScore1;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
+    // 3 checking numbers is not zero
+    if (rollDice !== 1) {
+      // Add dice to current score
+      currentScore += rollDice;
+      // currentE1.textContent = currentScore1;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    }
+
+    // if number is 1 then chnaging the player
+    else {
+      // switch the player
+      switchPlayer();
+    }
   }
+});
 
-  // if number is 1 then chnaging the player
-  else {
-    // switch the player
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
-    playerStyleE1.classList.toggle('player--active');
-    playerStyleE2.classList.toggle('player--active');
+// holding the score
+holdBtn.addEventListener('click', () => {
+  if (playing) {
+    // 1 Add current score to active player
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    // player win the game
+    if (scores[activePlayer] >= 20) {
+      playing = false;
+      console.log('paying condition', playing);
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      diceEl.classList.add('hidden');
+    } else {
+      switchPlayer();
+    }
   }
 });
